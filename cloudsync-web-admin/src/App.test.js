e7d3dcd5b5543bc97b500458d86ad9b8abb9403d
Juan Manuel Gonzalez <juanmg0511@users.noreply.github.com>
2022-07-03 18:53:22 -0300

    Updated basic test for home page

--- cloudsync-web-admin/src/App.test.js
+++ cloudsync-web-admin/src/App.test.js
@@ -1,9 +1,9 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', async () => {
+test('renders placeholder text', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/CloudSync/i);
   await waitFor(() => {
     expect(linkElement).toBeInTheDocument();
   });
