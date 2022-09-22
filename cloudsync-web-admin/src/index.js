320a29d5e17bc67c83357f0f29cbaee19160165a
Juan Manuel Gonzalez <juanmg0511@users.noreply.github.com>
2022-09-22 11:32:36 -0300

    Added https redirect

--- cloudsync-web-admin/src/index.js
+++ cloudsync-web-admin/src/index.js
@@ -2,6 +2,7 @@ import 'react-app-polyfill/stable'
 import 'core-js'
 import React from "react";
 import ReactDOM from "react-dom/client";
+import HttpsRedirect from 'react-https-redirect';
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from 'react-redux'
@@ -9,9 +10,11 @@ import store from './store'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-      <App />
-  </Provider>
+  <HttpsRedirect>
+    <Provider store={store}>
+        <App />
+    </Provider>
+  </HttpsRedirect>
 );
 
 // If you want to start measuring performance in your app, pass a function
