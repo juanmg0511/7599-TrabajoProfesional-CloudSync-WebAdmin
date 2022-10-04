a4b7091f03e6665393a5fcd72974f19deef523f6
Juan Manuel Gonzalez <juanmg0511@users.noreply.github.com>
2022-10-04 15:04:16 -0300

    Update index.js

--- cloudsync-web-admin/src/views/admin-users/edit/index.js
+++ cloudsync-web-admin/src/views/admin-users/edit/index.js
@@ -185,9 +185,11 @@ const AdminEdit = () => {
   }
 
   function parseTimestamp(timestamp) {
-    const date = new Date(timestamp)
-    return date.toUTCString()
-  }  
+      const date = new Date(timestamp)
+      const utcDate = new Date(date.getTime() - (date.getTimezoneOffset()*60000))
+
+    return utcDate.toUTCString()
+  }
 
   useEffect(() => {setupForm()}, [])
   return (
